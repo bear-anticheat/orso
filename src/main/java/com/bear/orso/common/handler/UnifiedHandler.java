0323312aff5cb391f4d4e19a7f48f63d3e4e9dfc
Aleks Xhuvani <kenhydrogen@gmail.com>
2026-09-02 04:59:52 +0200

    feat: handle alert regex

--- src/main/java/com/bear/orso/common/handler/UnifiedHandler.java
+++ src/main/java/com/bear/orso/common/handler/UnifiedHandler.java
@@ -4,19 +4,33 @@ import com.bear.bjornsdk.object.Configuration;
 import com.bear.orso.common.alert.MessageParser;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import lombok.experimental.UtilityClass;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@UtilityClass
 public class UnifiedHandler {
 
     private static final String ALERT_FORMAT =
-            "&b&l⚡ &r&7/ &b%player% &7has failed &b%name% &7(&b%type%&7) &3x%vl%";
+        "&b&l⚡ &r&7/ &b%player% &7has failed &b%name% &7(&b%type%&7) &3x%vl% {{&c&o(Experimental)}}";
 
-    public void handle(final Configuration configuration, final String _json,
-                       final BiConsumer<String, String> alertConsumer, final Consumer<String> banConsumer) {
+    private static final Pattern EXPERIMENTAL_CHECK_PATTERN = Pattern.compile("(?<=[{]{2})(.*)(?=[}]{2})");
+
+    private static final Map<String, String> ALERT_MAP = new HashMap<>();
+
+    static {
+        ALERT_MAP.put("null_false", computeAlertRegex(ALERT_FORMAT, false));
+        ALERT_MAP.put("null_true", computeAlertRegex(ALERT_FORMAT, true));
+    }
+
+    public static void handle(final Configuration configuration, String _json,
+                              final BiConsumer<String, String> alertConsumer, final Consumer<String> banConsumer) {
+        _json = _json.substring(2);
+
         final JsonObject json = JsonParser.parseString(_json).getAsJsonObject();
         final String type = json.get("type").getAsString();
 
@@ -26,7 +40,16 @@ public class UnifiedHandler {
             case "alert": {
                 if (isOnline && !configuration.isProxyAlerts()) break;
 
-                final String formatted = MessageParser.fromJson(json, isOnline ? configuration.getAlertFormat() : ALERT_FORMAT);
+                final String alertKey = (isOnline ? "bjorn" : "null") + "_" + json.get("check_developer").getAsBoolean();
+
+                if (isOnline && !ALERT_MAP.containsKey(alertKey)) {
+                    final String bjornFormat = new String(configuration.getAlertFormat().getBytes(), StandardCharsets.UTF_8);
+
+                    ALERT_MAP.put("bjorn_true", computeAlertRegex(bjornFormat, true));
+                    ALERT_MAP.put("bjorn_false", computeAlertRegex(bjornFormat, false));
+                }
+
+                final String formatted = MessageParser.fromJson(json, ALERT_MAP.get(alertKey));
                 final String data = json.get("data").getAsString();
 
                 alertConsumer.accept(formatted, data);
@@ -48,5 +71,21 @@ public class UnifiedHandler {
                 break;
             }
         }
+    }
+
+    private static String computeAlertRegex(String alertFormat, final boolean developer) {
+        final Matcher match = EXPERIMENTAL_CHECK_PATTERN.matcher(alertFormat);
+
+        if (match.find()) {
+            final String experimentalMessage = match.group();
+
+            if (!developer) {
+                alertFormat = alertFormat.replace("{{" + experimentalMessage + "}}", "");
+            } else {
+                alertFormat = alertFormat.replace("{{", "").replace("}}", "");
+            }
+        }
+
+        return alertFormat;
     }
 }
